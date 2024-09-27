daf3c8e1e7e442aecfe7b44a72d4de240dd0ad5c
elciosava <elciosava@outlook.com>
2024-09-27 11:11:16 -0300

    SFSDF

--- src/components/sobre_componente.vue
+++ src/components/sobre_componente.vue
@@ -9,9 +9,18 @@
 </template>
 
 <style scoped>
-*{
-    margin: 0;
-    padding: 0;
-    box-sizing: 0;
-}
+    *{
+        margin: 0;
+        padding: 0;
+        box-sizing: 0;
+    }
+    #container{
+        height: 100vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        align-content: center;
+        background-color: #F7F7F7;
+    }
 </style>
