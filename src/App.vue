c969d6c96da70a29d740dbc4d25c6b59d70ae139
elciosava <elciosava@outlook.com>
2024-09-26 22:09:44 -0300

    vai

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import MenuComponente from './components/menu_componente.vue'
-import inicioComponente from './components/inicioComponente.vue';
+import inicioComponente from './components/inicio_componente.vue';
 
 </script>
 
